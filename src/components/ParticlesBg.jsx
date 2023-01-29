07d43fee41ff40b6fed8167d3478c35edbe0575a
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-29 15:31:23 -0800

    create: particles component

---
+++ src/components/ParticlesBg.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './particlejs.min.js'
+import '../particlesjs-config.json';
+
+const ParticlesBg = () => {
+    return (
+        <>
+            <div id="particles-js" className="particles-js"></div>
+        </>
+    );
+};
+
+export default ParticlesBg;
