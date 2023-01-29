3bea1c57c875b837e5914fdd0052cd419fb0ca40
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-29 15:31:10 -0800

    typo: import change to change component name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Business from './components/Business'
-import Header from './components/Header'
+import Navbar from './components/Navbar'
 import slider1 from "./assets/img/slider1.png"
 import slider2 from "./assets/img/slider2.png"
 import slider3 from "./assets/img/slider3.png"
@@ -10,21 +10,21 @@ import logo from "./assets/img/logo_big.png"
 function App() {
 
 
-  return (
-    <div className="App">
-      <div className='presentation'>
-        <Header />
-        <div className='img_container'>
-          <img className='slider one' src={slider1} alt="" />
-          <img className='slider two' src={slider2} alt="" />
-          <img className='slider three' src={slider3} alt="" />
-          <img className='logo' src={logo} alt="" />
-        </div>
-        <Business />
-      </div>
+    return (
+        <div className="App">
+            <div className='presentation'>
+                <Navbar />
+                <div className='img_container'>
+                    <img className='slider one' src={slider1} alt="" />
+                    <img className='slider two' src={slider2} alt="" />
+                    <img className='slider three' src={slider3} alt="" />
+                    <img className='logo' src={logo} alt="" />
+                </div>
+                <Business />
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default App
