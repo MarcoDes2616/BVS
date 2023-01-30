7ba9ca28ae5cf6b0132280a2ad74a2b0498e107a
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-30 02:11:28 -0800

    add: section style

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ function App() {
         <div className="App">
             <Navbar />
             <Hero />
+            <section className='hi'>
+                <h1>Hola soy una seccion</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam beatae doloribus, nostrum suscipit dignissimos nobis cum at nam reiciendis non aut quibusdam. Ab nesciunt nobis consequatur voluptas accusamus odit ipsum.
+                </p>
+            </section>
         </div>
     )
 }
